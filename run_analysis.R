e5fa7e4f9ff1f48c5c5689ac173043c1fbf69b4e
malkiz <omieyal@gmail.com>
2020-11-01 21:43:55 +0200

    finish implementing

--- run_analysis.R
+++ run_analysis.R
@@ -18,8 +18,34 @@ X <- X[, names(X)[grepl('(mean|std)\\(\\)$', names(X))]]
 # Use descriptive activity names to name the activities in the data set
 activity_labels <- read.table('raw_data/activity_labels.txt', sep = "", col.names = c("index", "activity"))
 y$labels <- c(sapply(y, function(v) activity_labels$activity[v]))
-y <- y[, "labels"]
+
+# Merge the labels into the data set
+X$Activity <- y[, "labels"]
 
 # Appropriately label the data set with descriptive variable names.
+names_temp <- names(X)
+names_temp <- sub("^t", "Time", names_temp)
+names_temp <- sub("^f", "Frequency", names_temp)
+names_temp <- sub("Acc", "Acceleration", names_temp)
+names_temp <- sub("Mag", "Magnitude", names_temp)
+names_temp <- sub("-mean\\(\\)", "Mean", names_temp)
+names_temp <- sub("-std\\(\\)", "StandardDeviation", names_temp)
+names(X) <- names_temp
 
-# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# Add the subject to the data set
+subject_train <- read.table('raw_data/train/subject_train.txt', sep = "")
+subject_test <- read.table('raw_data/test/subject_test.txt', sep = "")
+subjects <- rbind(subject_train, subject_test)
+X$Subject <- subjects[, 1]
+
+# From the data set in step 4, create a second, independent tidy data set with the average of each variable for each activity and each subject.
+mean_by_activity <- X %>% select(-Subject) %>% group_by(Activity) %>% summarise(across(1:18, mean, na.rm= TRUE))
+mean_by_subject <- X %>% select(-Activity) %>% group_by(Subject) %>% summarise(across(1:18, mean, na.rm= TRUE))
+
+# save data sets to files
+if (!dir.exists("result")) {
+  dir.create("result")
+}
+write.csv(X, "result/dataset.csv")
+write.csv(mean_by_activity, "result/mean_by_activity.csv")
+write.csv(mean_by_subject, "result/mean_by_subject.csv")
